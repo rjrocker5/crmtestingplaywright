867abbcf88ac70d16dd793f10c54c43e8ee95866
Israr <>
2026-03-29 16:05:56 +0530

    Fixed small bug in Playwright test

--- tests/crmtest.spec.js
+++ tests/crmtest.spec.js
@@ -4,8 +4,8 @@ test('to create new user account', async ({ page }) => {
  await page.goto('https://rahulshettyacademy.com/client/#/auth/register');
  await page.locator('//input[@id="firstName"]').fill('Israr');
  await page.getByPlaceholder('Last Name').fill('Hussain');
- await page.getByPlaceholder('email@example.com').fill('isrikd1r@example.com');
- await page.getByPlaceholder('enter your number').fill('7992323766');
+ await page.getByPlaceholder('email@example.com').fill('iskkd1r@example.com');
+ await page.getByPlaceholder('enter your number').fill('7992323760');
  await page.locator('//select[@formcontrolname="occupation"]').selectOption('Engineer');
  await page.locator('//input[@value="Male"]').check();
  await page.locator('//input[@id="userPassword"]').fill('Aaa@12345');
@@ -13,5 +13,6 @@ test('to create new user account', async ({ page }) => {
  await page.locator('//input[@formcontrolname="required"]').check();
  await page.locator('//input[@type="submit"]').click();
   await page.waitForTimeout(6000);
+    await page.context().close();
 });
 
