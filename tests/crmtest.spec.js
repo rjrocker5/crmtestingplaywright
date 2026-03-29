e0aae96a3a9ab6e96e5dcba7730767f6787fa3b9
Israr <>
2026-03-29 15:27:47 +0530

    first commit

---
+++ tests/crmtest.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('to create new user account', async ({ page }) => {
+ await page.goto('https://rahulshettyacademy.com/client/#/auth/register');
+ await page.locator('//input[@id="firstName"]').fill('Israr');
+ await page.getByPlaceholder('Last Name').fill('Hussain');
+ await page.getByPlaceholder('email@example.com').fill('isrikd1r@example.com');
+ await page.getByPlaceholder('enter your number').fill('7992323766');
+ await page.locator('//select[@formcontrolname="occupation"]').selectOption('Engineer');
+ await page.locator('//input[@value="Male"]').check();
+ await page.locator('//input[@id="userPassword"]').fill('Aaa@12345');
+ await page.locator('//input[@id="confirmPassword"]').fill('Aaa@12345');
+ await page.locator('//input[@formcontrolname="required"]').check();
+ await page.locator('//input[@type="submit"]').click();
+  await page.waitForTimeout(6000);
+});
+
